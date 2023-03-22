ae31ba38290f91fa0bed7102e3e2f664b54b5c18
GabrielleHieken <gabriellehieken@gmail.com>
2023-03-21 20:46:35 -0500

    testing one child, one second at a time

--- oss.c
+++ oss.c
@@ -26,7 +26,11 @@ typedef struct msgbuffer {
 int main(int argc, char *argv[]){
 
     //My constant time quantum
+    int q = 15200;
 
+    //max random time user processes will be created between each process
+    int maxSec = 1;
+    int maxNano;
 
     //default logfile name
     char* logFile = "logfile";
@@ -85,60 +89,74 @@ int main(int argc, char *argv[]){
 
     printf("Message queue set up\n");
 
-    // store pids of our first two children to launch
-    pid_t child[0];
-    int i = 0;
+    while(1) {// store pids of our first two children to launch
+        int childNum = 0;
+        pid_t child[childNum];
+        
+        int i = 0;
+        // create our child
+        for (i = 0; i < 1; i++) {
+            // lets fork off a child
+            pid_t pid = fork();
 
-    // create our two children
-    for (i = 0; i < 1; i++) {
-        // lets fork off a child
-        pid_t pid = fork();
+            if (pid > 0) {
+                // save this child's pid
+                child[i] = pid;
+            }
+            else if (pid == 0) {
+                // in child, so lets exec off child executable
+                execlp("./worker","./worker",(char *)NULL);
 
-        if (pid > 0) {
-            // save this child's pid
-            child[i] = pid;
+                // should never get here if exec worked
+                printf("Exec failed for first child\n");
+
+                exit(1);
+            }
+            else {
+                // fork error
+                perror("fork failed in parent");
+            }
         }
-        else if (pid == 0) {
-            // in child, so lets exec off child executable
-            execlp("./worker","./worker",(char *)NULL);
 
-            // should never get here if exec worked
-            printf("Exec failed for first child\n");
+        char buf[7];
+        int j;
+        for(j = 0; j < 10; j++){
+            snprintf(buf, childNum, "buf%d", j);
+        }
+        // lets send a message only to child1, not child0
 
+        buf.mtype = child[childNum];
+        buf.intData = child[childNum]; // we will give it the pid we are sending to, so we know it received it
+        
+        strcpy(buf.strData,"Message to child 1\n");
+        
+        //send message to worker process
+        if (msgsnd(msqid, &buf, sizeof(msgbuffer)-sizeof(long), 0) == -1) {
+            perror("msgsnd to child 1 failed\n");
             exit(1);
         }
-        else {
-            // fork error
-            perror("fork failed in parent");
+
+        msgbuffer rcvbuf;
+
+        // Then let me read a message, but only one meant for me
+        // ie: the one the child just is sending back to me
+        if (msgrcv(msqid, &rcvbuf,sizeof(msgbuffer), getpid(),0) == -1) {
+            perror("failed to receive message in parent\n");
+            exit(1);
         }
-    }
 
-    // lets send a message only to child1, not child0
-    buf0.mtype = child[0];
-    buf0.intData = child[0]; // we will give it the pid we are sending to, so we know it received it
-    
-    strcpy(buf0.strData,"Message to child 1\n");
-    
-    //send message to worker process
-    if (msgsnd(msqid, &buf0, sizeof(msgbuffer)-sizeof(long), 0) == -1) {
-        perror("msgsnd to child 1 failed\n");
-        exit(1);
-    }
+        printf("Parent %d received message: %s my int data was %d\n",getpid(),rcvbuf.strData,rcvbuf.intData);
 
-    msgbuffer rcvbuf;
-
-    // Then let me read a message, but only one meant for me
-    // ie: the one the child just is sending back to me
-    if (msgrcv(msqid, &rcvbuf,sizeof(msgbuffer), getpid(),0) == -1) {
-        perror("failed to receive message in parent\n");
-        exit(1);
-    }
-
-    printf("Parent %d received message: %s my int data was %d\n",getpid(),rcvbuf.strData,rcvbuf.intData);
-
-    // wait for children to end
-    for (i = 0; i < 2; i++) {
-        wait(0);
+        // wait for children to end
+        for (i = 0; i < 2; i++) {
+            wait(0);
+        }
+        
+        i++;
+        sleep(maxSec);
+        if(i == 3){
+            break;
+        }
     }
 
     // get rid of message queue
@@ -146,7 +164,6 @@ int main(int argc, char *argv[]){
         perror("msgctl to get rid of queue in parent failed");
         exit(1);
     }
-    
 
     //close the log file
     fclose(fileLogging);
