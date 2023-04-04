caed1a106e1d05b7c7e3534620d0bc2c6a16f078
GabrielleHieken <gabriellehieken@gmail.com>
2023-04-04 16:06:01 -0500

    format table

--- oss.c
+++ oss.c
@@ -466,7 +466,7 @@ void printTable(FILE* fileLogging){
             break;
         }
         
-        printf("%i\t\t%i\t\t%d\t\t%d\t\t\t%i\t\t\t%i\t%f\n", i, processTable[i].occupied, (long)processTable[i].pid, (long)processTable[i].sim_pid, processTable[i].processNum,processTable[i].total_CPU_time, processTable[i].total_system_time);
-        fprintf(fileLogging, "%i\t%i\t\t%d\t\t%d\t%i\t\t\t%i\t%f\n", i, processTable[i].occupied, (long)processTable[i].pid, (long)processTable[i].sim_pid, processTable[i].processNum,processTable[i].total_CPU_time, processTable[i].total_system_time);     
+        printf("%i\t\t%i\t\t%d\t\t%d\t\t\t%i\t\t\t%i\t\t\t%f\n", i, processTable[i].occupied, (long)processTable[i].pid, (long)processTable[i].sim_pid, processTable[i].processNum,processTable[i].total_CPU_time, processTable[i].total_system_time);
+        fprintf(fileLogging, "%i\t%i\t\t%d\t\t%d\t%i\t\t\t%i\t\t\t%f\n", i, processTable[i].occupied, (long)processTable[i].pid, (long)processTable[i].sim_pid, processTable[i].processNum,processTable[i].total_CPU_time, processTable[i].total_system_time);     
     }
 }
