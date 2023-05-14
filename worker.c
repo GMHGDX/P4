ace7639f729b47ff655d6976c8f3d951700bc8c0
GabrielleHieken <gabriellehieken@gmail.com>
2023-05-14 16:22:27 -0500

    print frame table

--- worker.c
+++ worker.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]){
     int msqid = 0;
     key_t key;
 
-    srand(time(NULL)); //gets a random number for each child instead of the same
+    //srand(time(NULL)); //gets a random number for each child instead of the same
 
     // get the key for our message queue
     if ((key = ftok("oss.c", 1)) == -1) { perror("ftok"); exit(1); }
